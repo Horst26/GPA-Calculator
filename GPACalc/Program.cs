a98cb79523a261539a6829e3079fcba772c098ee
Horst26 <201503@pennmanor.net>
2021-04-19 08:18:20 -0400

    Added the basic ability to read from a text file and split it into individual inputs.

---
+++ GPACalc/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace GPACalc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Creates path to text file containing user input
+            string filepath = @"/home/user/Documents/GPA.txt";
+
+            string fulltext = File.ReadAllText(filepath);
+
+            //Splits all of the inputs into individual values
+            string[] split = fulltext.Split(' ');
+
+            List<string> final = new List<string>();
+
+        }
+    }
+}
